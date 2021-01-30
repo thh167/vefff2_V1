4b7c69870c088148eaeb467d9f01f0b35ea92d5f
thh167 <32545064+thh167@users.noreply.github.com>
2021-01-30 18:55:40 +0000

    nýjustu breytingar

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { router } from './videos.js';
+import {router} from './videos.js';
+
+
 
 const app = express();
 
@@ -24,9 +26,42 @@ app.get('/videos', (req, res) => {
   res.render('videos', { title: 'Um', staff, extra });
 });
 
+app.use('/', router);
+
 const hostname = '127.0.0.1';
 const port = 3001;
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+/**
+ * Middleware sem sér um 404 villur.
+ *
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {function} next Næsta middleware
+ */
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  const title = 'Fannst ekki';
+  const message = 'Ó nei, efnið finnst ekki!';
+  res.status(404).render('error', { title, message });
+}
+
+/**
+ * Middleware sem sér um villumeðhöndlun.
+ *
+ * @param {object} err Villa sem kom upp
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {function} next Næsta middleware
+ */
+function errorHandler(err, req, res, next) { // eslint-disable-line
+  console.error(err);
+  const title = 'Villa kom upp';
+  const message = '';
+  res.status(500).render('error', { title, message });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
--- videos.js
+++ videos.js
@@ -1,7 +1,93 @@
 import express from 'express';
+import util from 'util';
+import fs from 'fs';
 
-export const router = express.Router();
+const readFileAsync = util.promisify(fs.readFile);
 
+
+
+/**
+ * Higher-order fall sem umlykur async middleware með villumeðhöndlun.
+ *
+ * @param {function} fn Middleware sem grípa á villur fyrir
+ * @returns {function} Middleware með villumeðhöndlun
+ */
+function catchErrors(fn) {
+  return (req, res, next) => fn(req, res, next).catch(next);
+}
+
+/**
+ * Les gögn async úr JSON skrá.
+ *
+ * @returns {object} Gögnum úr JSON skrá
+ */
+async function readList() {
+  // hér væri líka hægt að gera bara `require('./lecrures.json')`  en sýnum
+  // hvernig skjal lesið og JSON unnið
+  const file = await readFileAsync('./videos.json');
+
+  const json = JSON.parse(data);
+
+  return json;
+}
+
+/**
+ * Route handler sem birtir lista af vídjóum.
+ *
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ */
+async function list(req, res) {
+  const title = 'Vídjó';
+  const json = await readList();
+  const { videos } = json;
+
+  res.render('videos', { title, videos });
+}
+
+
+/**
+ * Route handler sem birtir fyrirlestur. Ef fyrirlestur finnst ekki í JSON skrá
+ * er kallað í next() sem mun enda í 404 handler.
+ *
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {function} next Næsta middleware
+ */
+async function video(req, res, next) {
+  const { slug } = req.params;
+
+  const json = await readList();
+  const { lectures } = json;
+
+  const foundLecture = lectures.find(a => a.slug === slug);
+
+  if (!foundLecture) {
+    // sendum í 404 handler
+    return next();
+  }
+
+  const { title } = foundLecture;
+
+  return res.render('video', { title, video: foundLecture });
+}
+
+router.get('/', catchErrors(list));
+router.get('/:slug', catchErrors(video));
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////              ///////////////////////////////              //////////////////////////
+/*
 router.get('/', (req, res) => {
   res.send('Hvað er í gangi?!');
 });
@@ -16,3 +102,5 @@ router.get(/foo.*$/, (req, res) => {
 router.get('/bar/:data?', (req, res) => {
   res.send(`Data = ${req.params.data}`);
 });
+
+ */
